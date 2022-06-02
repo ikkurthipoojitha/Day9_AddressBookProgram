63d87da0a5c52a81fff8aa8c3d15476730385f9a
poojitha <ipoojitha11997@gmail.com>
2022-06-02 13:34:27 +0530

    uc2

--- src/com/bridgelabz/AddressBook/AddressBook.java
+++ src/com/bridgelabz/AddressBook/AddressBook.java
@@ -1,5 +1,52 @@
 package com.bridgelabz.AddressBook;
 
-public class AddressBook {
+import java.util.ArrayList;
 
-}
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Iterator;
+import java.util.Scanner;
+
+public class AddressBook implements AddressBookInterface{
+    Scanner s = new Scanner(System.in);
+    ArrayList<Person> list = new ArrayList<Person>();
+
+
+    public void operation(ArrayList<Person> arrayRead) {
+        // list=AddressManager.listReturn();
+        list = arrayRead;
+        boolean status = true;
+        add();
+    }
+
+
+    public void add() {
+        Person person = new Person();
+        Address address = new Address();
+        System.out.println("Enter the First name:");
+        String fname = s.next();
+        person.setFname(fname);
+
+        System.out.println("Enter the Last name:");
+        String lname = s.next();
+        person.setLname(lname);
+
+        System.out.println("Enter the Phone Number:");
+        Long phone = s.nextLong();
+        person.setPhonenumber(phone);
+
+        System.out.println("Enter the City:");
+        String city = s.next();
+        address.setCity(city);
+
+        System.out.println("Enter the Zip:");
+        long zip = s.nextLong();
+        address.setZip(zip);
+
+        System.out.println("Enter the State:");
+        String state = s.next();
+        address.setState(state);
+        person.setAddress(address);
+        list.add(person);
+
+    }
