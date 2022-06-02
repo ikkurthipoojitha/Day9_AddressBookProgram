b0adf7447e69fed084adf3de2315d1dd01ccb6b2
poojitha <ipoojitha11997@gmail.com>
2022-06-02 13:25:30 +0530

    second

---
+++ src/com/bridgelabz/AddressBook/AddressBook.java
@@ -0,0 +1,5 @@
+package com.bridgelabz.AddressBook;
+
+public class AddressBook {
+
+}
--- src/com/bridgelabz/AddressBook/Person.java
+++ src/com/bridgelabz/AddressBook/Person.java
@@ -1,2 +1,54 @@
-package com.bridgelabz.AddressBook;public class Person {
+package com.bridgelabz.AddressBook;
+
+public class Person {
+    private String fname;
+    private String lname;
+    private long phonenumber;
+
+    private String mail;
+    private Address address;
+
+    public long getPhonenumber() {
+        return phonenumber;
+    }
+
+    public void setPhonenumber(long phonenumber) {
+        this.phonenumber = phonenumber;
+    }
+
+    public String getFname() {
+        return fname;
+    }
+
+    public void setFname(String fname) {
+        this.fname = fname;
+    }
+
+    public String getLname() {
+        return lname;
+    }
+
+    public void setLname(String lname) {
+        this.lname = lname;
+    }
+    public String getMail() {
+        return mail;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+    public String toString() {
+        return "Person [fname=" + fname + ", lname=" + lname + ", mail = "+ mail +", address="
+                + address + "]";
+    }
+
 }
